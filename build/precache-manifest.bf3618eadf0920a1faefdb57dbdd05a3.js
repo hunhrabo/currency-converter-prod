d44119797c77ce8e588ae0ee0c0e797d5a2fc4e9
tamas <hrabotam@gmail.com>
2019-05-14 19:14:46 +0300

    added frontend

---
+++ build/precache-manifest.bf3618eadf0920a1faefdb57dbdd05a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05f5df599b6509a62b6421595bbfd992",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11b29e48a2ae2831d188",
+    "url": "/static/css/main.c8198d49.chunk.css"
+  },
+  {
+    "revision": "f8be0bd1ecf78e005216",
+    "url": "/static/js/2.7e668c56.chunk.js"
+  },
+  {
+    "revision": "11b29e48a2ae2831d188",
+    "url": "/static/js/main.0e70e04e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ build/precache-manifest.fc7a67417d416813d95c88e3f4829ec1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff534a19242ce408a789daba8a2b8b30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40e3de5dc3a55d5556a7",
+    "url": "/static/css/main.c8198d49.chunk.css"
+  },
+  {
+    "revision": "f8be0bd1ecf78e005216",
+    "url": "/static/js/2.7e668c56.chunk.js"
+  },
+  {
+    "revision": "40e3de5dc3a55d5556a7",
+    "url": "/static/js/main.738033ef.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
