4b81b6c641c366626dc6896b2e48f3e401d3ac81
tamas <hrabotam@gmail.com>
2019-05-14 15:01:24 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const request = require("request");
+
+app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.get("/api/get", function(req, res) {
+  request(
+    {
+      method: "GET",
+      uri: "http://api.napiarfolyam.hu?bank=mnb"
+    },
+    function(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        res.send(body);
+      }
+    }
+  );
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
