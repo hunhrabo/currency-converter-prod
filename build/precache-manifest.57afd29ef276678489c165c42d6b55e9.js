c94b7249ada70bcce1e1c486b1734e911bf179d4
tamas <hrabotam@gmail.com>
2019-05-15 11:08:27 +0300

    fixed css issues

---
+++ build/precache-manifest.57afd29ef276678489c165c42d6b55e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d2f1e2039e0a724defe649910356e0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dbe9a1f040191c31f7a",
+    "url": "/static/css/main.68bcad84.chunk.css"
+  },
+  {
+    "revision": "f8be0bd1ecf78e005216",
+    "url": "/static/js/2.7e668c56.chunk.js"
+  },
+  {
+    "revision": "6dbe9a1f040191c31f7a",
+    "url": "/static/js/main.34548012.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fc7a67417d416813d95c88e3f4829ec1.js"
+  "/precache-manifest.57afd29ef276678489c165c42d6b55e9.js"
 );
 
 self.addEventListener('message', (event) => {
